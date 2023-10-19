17584ccbb5a25fdb2c7a5cd939a65021ea9549c5
RVPdev <renatovilcahuaman@gmail.com>
2023-10-18 23:31:42 -0500

    adding documentation

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,34 @@
+// Include the Raylib header for access to its functions and types
 #include <raylib.h>
 
+// Entry point for the program
 int main()
 {
+    // Define the screen dimensions
     const int screen_width = 1280;
-    const int screen_heigth = 800;
+    const int screen_height = 800;
 
-    InitWindow(screen_width, screen_heigth, "My Pong Game");
+    // Initialize the window with the given dimensions and title
+    InitWindow(screen_width, screen_height, "My Pong Game");
+
+    // Set the target frames per second (FPS)
     SetTargetFPS(60);
 
+    // Main game loop; continues until the window is closed
     while (WindowShouldClose() == false)
     {
-
+        // Begin the drawing phase
         BeginDrawing();
 
+        // (Your game logic and drawing code will go here)
+
+        // End the drawing phase
         EndDrawing();
     }
 
+    // Close the window and free up resources
     CloseWindow();
 
+    // Return 0 to indicate successful execution
     return 0;
 }
